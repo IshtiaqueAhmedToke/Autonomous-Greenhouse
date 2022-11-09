22787a8fda3bc2ccc9d19ca4c398038b3fed9fb9
Ishtiaque Ahmed <1720943@iub.edu.bd>
2022-11-10 01:54:58 +0600

    Update GreenHouse.ino

--- GreenHouse.ino
+++ GreenHouse.ino
@@ -1,18 +1,28 @@
 #include <dht.h>
-
-
+ 
+ 
 #define dht_apin A0 // dht pin
 byte serialA;
+ 
+ 
+// MOISTURE SENSOR SMOOTHING ALGORITHM
+const int numReadings = 30;
+ 
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+ 
+const int sensor_pin = A3;
 
-int analogPin = A3; //moisture sensor
+
 int val = 0;  // variable to store the value read
 int moisturePercentage = 0;
-const int  dry = 599;
-const int wet = 216;
 
-
- int relay1 = 2; 
- int relay2 = 3;
+ 
+ 
+ int relay1 = 2;
+ int relay2 = 4;
  
  dht DHT;
  
@@ -21,53 +31,77 @@ void setup(){
   Serial.begin(9600);
  pinMode(relay1,OUTPUT);
  pinMode(relay2, OUTPUT);
- 
+
+ // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
  
  
 }
  
 void loop(){
-  
+ 
  
     DHT.read11(dht_apin);
-    val = analogRead(analogPin);  
-    moisturePercentage = map(val,wet,dry,100,0);
-    
+   
 
+ float moisture_percentage;
+  int sensor_analog;
 
-
+  sensor_analog = analogRead(sensor_pin);
+  moisture_percentage = abs(( 100 - ( (sensor_analog/1023.00) * 100 ) ));
+ 
+ 
+ 
+ 
+ 
     Serial.print("Humidity= ");
     Serial.println(DHT.humidity);
-  
+ 
     Serial.print("Temperature= ");
-    Serial.println(DHT.temperature); 
-
+    Serial.println(DHT.temperature);
+ 
     Serial.print("SoilMoisture=  ");
-    Serial.println(  moisturePercentage);      
-      delay(400);
+    Serial.println(moisture_percentage - 47);
+      delay(350);
        // debug value
-    
-
- if (Serial.available() > 0) {serialA = Serial.read();Serial.println(serialA);} 
-
+   
+ 
+ if (Serial.available() > 0) {serialA = Serial.read();Serial.println(serialA);}
+ 
  switch(serialA)
  {
-  
+ 
   case '1':
   digitalWrite(relay1,HIGH);
+  
+  break;
+ 
+  case '2':
+  digitalWrite(relay1,LOW);
+
+  break;
+
+
+
+   case '3':
+ 
   digitalWrite(relay2,HIGH);
   break;
 
-  case '2':
-  digitalWrite(relay1,LOW);
+
+
+   case '4':
+
   digitalWrite(relay2,LOW);
   break;
-
-
+ 
+ 
    
   }
-
-
-
+ 
+ 
+ 
  
 }
