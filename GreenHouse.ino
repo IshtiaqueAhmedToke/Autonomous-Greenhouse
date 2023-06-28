302eb7311553baeb2f6fe84de3c9c0031b6a8655
Ishtiaque Ahmed <1720943@iub.edu.bd>
2023-06-28 07:08:57 +0600

    FINAL GreenHouse.ino

--- GreenHouse.ino
+++ GreenHouse.ino
@@ -1,107 +1,155 @@
-#include <dht.h>
- 
- 
-#define dht_apin A0 // dht pin
-byte serialA;
- 
- 
-// MOISTURE SENSOR SMOOTHING ALGORITHM
-const int numReadings = 30;
- 
-int readings[numReadings];      // the readings from the analog input
-int readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average
- 
-const int sensor_pin = A3;
+#include <DHT.h>
 
+// Soil moisture sensors
+const int soilMoisturePin1 = A0;
+const int soilMoisturePin2 = A1;
+const int soilMoisturePin3 = A2;
 
-int val = 0;  // variable to store the value read
-int moisturePercentage = 0;
+// CO sensor
+const int coSensorPin = A3;
 
- 
- 
- int relay1 = 2;
- int relay2 = 4;
- 
- dht DHT;
- 
-void setup(){
- 
+// pH sensor
+const int phSensorPin = A4;
+
+// DHT11 sensor
+const int dht11Pin = 2;
+#define DHTTYPE DHT11
+DHT dht(dht11Pin, DHTTYPE);
+
+// Control Pins
+const int nutrientPumpPin = 3;
+const int waterPumpPin = 4;
+const int exhaustFanPin = 5;
+
+// pH sensor simulation
+float pH;
+//actual ph
+float phValue;
+
+void setup() {
   Serial.begin(9600);
- pinMode(relay1,OUTPUT);
- pinMode(relay2, OUTPUT);
+  dht.begin();
 
- // initialize all the readings to 0:
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings[thisReading] = 0;
-  }
- 
- 
+  // Set control pins as OUTPUT
+  pinMode(nutrientPumpPin, OUTPUT);
+  pinMode(waterPumpPin, OUTPUT);
+  pinMode(exhaustFanPin, OUTPUT);
 }
- 
-void loop(){
- 
- 
-    DHT.read11(dht_apin);
-   
 
- float moisture_percentage;
-  int sensor_analog;
+void loop() {
+  // Read soil moisture sensors
+  int soilMoisture1 = analogRead(soilMoisturePin1);
+  int soilMoisture2 = analogRead(soilMoisturePin2);
+  int soilMoisture3 = analogRead(soilMoisturePin3);
 
-  sensor_analog = analogRead(sensor_pin);
-  moisture_percentage = abs(( 100 - ( (sensor_analog/1023.00) * 100 ) ));
- 
- 
- 
- 
- 
-    Serial.print("Humidity= ");
-    Serial.println(DHT.humidity);
- 
-    Serial.print("Temperature= ");
-    Serial.println(DHT.temperature);
- 
-    Serial.print("SoilMoisture=  ");
-    Serial.println(moisture_percentage - 47);
-      delay(350);
-       // debug value
-   
- 
- if (Serial.available() > 0) {serialA = Serial.read();Serial.println(serialA);}
- 
- switch(serialA)
- {
- 
-  case '1':
-  digitalWrite(relay1,HIGH);
-  
-  break;
- 
-  case '2':
-  digitalWrite(relay1,LOW);
+  // Read CO sensor
+  int coRaw = analogRead(coSensorPin);
+  float co_ppm = calculateCOppm(coRaw);
 
-  break;
+  // Read DHT11 sensor
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
 
+  // Read actual pH sensor
+  phValue = readPhSensor();
 
+  // Simulate pH sensor
+  pH = simulatePhSensor(soilMoisture1, soilMoisture2, soilMoisture3, co_ppm, temperature, humidity);
 
-   case '3':
- 
-  digitalWrite(relay2,HIGH);
-  break;
+  // Control nutrient pump, water pump, and exhaust fan
+  controlSystem(temperature, soilMoisture1, soilMoisture2, soilMoisture3, phValue);
 
+  // Manual control of nutrient pump, water pump, and exhaust fan
+  if (Serial.available() > 0) {
+    char serialA = Serial.read();
+    Serial.println(serialA);
 
+    switch(serialA) {
+      case '1':
+        digitalWrite(nutrientPumpPin, HIGH); // Turn on nutrient pump
+        break;
+      
+      case '2':
+        digitalWrite(nutrientPumpPin, LOW); // Turn off nutrient pump
+        break;
+        
+      case '3':
+        digitalWrite(waterPumpPin, HIGH); // Turn on water pump
+        break;
 
-   case '4':
+      case '4':
+        digitalWrite(waterPumpPin, LOW); // Turn off water pump
+        break;
 
-  digitalWrite(relay2,LOW);
-  break;
- 
- 
-   
+      case '5':
+        digitalWrite(exhaustFanPin, HIGH); // Turn on exhaust fan
+        break;
+
+      case '6':
+        digitalWrite(exhaustFanPin, LOW); // Turn off exhaust fan
+        break;
+
+      default:
+        Serial.println("Invalid command");
+    }
   }
- 
- 
- 
- 
+
+  // Print readings
+  Serial.print("Soil Moisture 1 (%): "); Serial.println(soilMoisture1);
+  Serial.print("Soil Moisture 2 (%): "); Serial.println(soilMoisture2);
+  Serial.print("Soil Moisture 3 (%): "); Serial.println(soilMoisture3);
+  Serial.print("CO (ppm): "); Serial.println(co_ppm);
+  Serial.print("Temperature (C): "); Serial.println(temperature);
+  Serial.print("Humidity (%): "); Serial.println(humidity);
+  Serial.print("Simulated pH: "); Serial.println(pH);
+  Serial.print("Actual pH: "); Serial.println(phValue);
+
+  delay(2000);
+}
+
+void controlSystem(float temperature, int soilMoisture1, int soilMoisture2, int soilMoisture3, float phValue) {
+  // Control logic based on temperature, soil moisture and pH value
+  // Activate nutrient pump if pH value is not within optimal range (5.5 to 7.0)
+  if (phValue < 5.5 || phValue > 7.0) {
+    digitalWrite(nutrientPumpPin, HIGH);
+  } else {
+    digitalWrite(nutrientPumpPin, LOW);
+  }
+
+  // Activate water pump if all of the soil moisture readings are below 40%
+  if (soilMoisture1 < 40 && soilMoisture2 < 40 && soilMoisture3 < 40) {
+    digitalWrite(waterPumpPin, HIGH);
+  } else {
+    digitalWrite(waterPumpPin, LOW);
+  }
+
+  // Activate exhaust fan if temperature is above 30 degrees Celsius
+  if (temperature > 30) {
+    digitalWrite(exhaustFanPin, HIGH);
+  } else {
+    digitalWrite(exhaustFanPin, LOW);
+  }
+}
+
+float calculateCOppm(int coRaw) {
+  float sensorVoltage = (float)coRaw / 1024 * 5.0;
+  float sensorResistance = (5.0 - sensorVoltage) * 10 / sensorVoltage;
+  float ro = 10; // Reference resistance in clean air (in kOhms)
+  float sensitivity = 0.6; // Sensitivity factor (varies depending on the sensor)
+  float rs_ro_ratio = sensorResistance / ro;
+  float co_ppm = pow((rs_ro_ratio / sensitivity), (1 / -1.179));
+  return co_ppm;
+}
+
+float simulatePhSensor(int soilMoisture1, int soilMoisture2, int soilMoisture3, float co_ppm, float temperature, float humidity) {
+  float avgSoilMoisture = (soilMoisture1 + soilMoisture2 + soilMoisture3) / 3.0;
+  float pH = 6.0 + ((avgSoilMoisture / 100.0) * 1.5) - (co_ppm / 1000.0) + (temperature / 100.0) - (humidity / 100.0);
+  return constrain(pH, 0, 14);
+}
+
+float readPhSensor() {
+  int phRaw = analogRead(phSensorPin);
+  // Convert the raw analog value to pH using calibration parameters.
+  float phValue = (float)phRaw / 1024 * 5.0 * 3.5; 
+  return phValue;
 }
